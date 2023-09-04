185b7c148e4314c4168c3e1bb8d7dc0ce0deb91e
Adrian Ceballos <78570037+adceballos@users.noreply.github.com>
2023-09-03 20:42:51 -0500

    Create 345.cpp

---
+++ LeetCode75/Array-String/345.cpp
@@ -0,0 +1,27 @@
+// Reverse Vowels of a String
+// used solution
+class Solution {
+public:
+    string reverseVowels(string s) {
+        int left = 0;
+        int right = s.size() - 1;
+        while (left < right) {
+            while ((left < right) && !isVowel(s[left])) {
+                left++;
+            }
+            while ((left < right) && !isVowel(s[right])) {
+                right--;
+            }
+            if (left < right) {
+                swap(s[left], s[right]);
+                left++;
+                right--;
+            }
+        }
+        return s;
+    }
+private:
+    bool isVowel(char c) const {
+        return c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u' || c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U';
+    }
+};
